bd3579b482233765b49224a52dc711ea23bfb5cb
tarz2929 <saitarunmulley@gmail.com>
2020-10-07 14:38:08 -0600

    first commit

---
+++ techcareers-calc.php
@@ -0,0 +1,13 @@
+<?php 
+/**
+ * Plugin Name:       TECHCareers Calculator
+ * Plugin URI:        
+ * Description:       Simple php calculator
+ * Version:           1.0
+ * Requires at least: 5
+ * Requires PHP:      7.4
+ * Author:            Tarun Mulle
+ * Author URI:       
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ */
